0ec29255a0a4258dd2956266d71a329ccd7f448d
PedroGarcia1227 <pedrohenriquegdn15@gmail.com>
2024-06-22 13:52:53 -0300

    Create Model ProductCategory

---
+++ Models/ProductCategory.cs
@@ -0,0 +1,20 @@
+public class ProductCategory
+{
+    public required int ProductCategoryID
+    {
+        get;
+        set;
+    }
+
+    public required string Name
+    {
+        get;
+        set;
+    }
+
+    public string? Description
+    {
+        get;
+        set;
+    }
+}
--- obj/Debug/net8.0/api-mvc-codefirst.AssemblyInfo.cs
+++ obj/Debug/net8.0/api-mvc-codefirst.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api-mvc-codefirst")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4251803073c5206d0395c40ddbf9552a47c985c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac92d297d4df5681c87dda871bee7a6554380837")]
 [assembly: System.Reflection.AssemblyProductAttribute("api-mvc-codefirst")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api-mvc-codefirst")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
