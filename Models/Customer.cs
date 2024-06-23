3a0ce724cb966f757b56c00e4c63f3747c1f2c02
PedroGarcia1227 <pedrohenriquegdn15@gmail.com>
2024-06-22 23:58:46 -0300

    Update Model Customer

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,7 +1,6 @@
 public class Customer
 {
     public required int CustomerID { get; set; }
-
     public required string Name { get; set; }
     public required string Address { get; set; }
     public required string City { get; set; }
